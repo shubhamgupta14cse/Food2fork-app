11e5ac4abee375f575ceacc069732339962b3b14
Shubham Gupta <shubhamgupta14cse@users.noreply.github.com>
2018-09-11 09:55:00 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,15 +1,11 @@
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
-const a='462b1cc8d4f2730081462fbc65136320';
-const b='831f339c897a7e0167842b1c83ea2ecb';
-const c='7f19e15b753064e9f917a6d0a1b3b294';
-const d='5120a841ab0543c7572a2581bb41e09d';
-const e='083b367cc0d3fc4abeae116ad2f008db';
-const f='f5a15fc17b9bb8d798d177daa9330292';
-const g='c8ca242a88882bccd2c418bc27d6f272';
-const h='49e58fad1f1554058547a539055f8b71';
-const i='5015470b83b12feb5afb7481d6ad0fd8';
-const arr=[a,b,c,d,e,f,g,h,i];
-//export const key = '462b1cc8d4f2730081462fbc65136320';
-//5120a841ab0543c7572a2581bb41e09d//083b367cc0d3fc4abeae116ad2f008db//f5a15fc17b9bb8d798d177daa9330292//
-//c8ca242a88882bccd2c418bc27d6f272//49e58fad1f1554058547a539055f8b71//5015470b83b12feb5afb7481d6ad0fd8
+const a=//paste your key here//
+const b=//paste your key here//
+const c=//paste your key here//
+const d=//paste your key here//
+      
+      
+
+const arr=[a,b,c,d];
+
 export const key=index=> arr[index];
